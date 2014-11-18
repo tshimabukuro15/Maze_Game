848e0e91412ec6fdd4cbdfd658a39dfcc920f3da
tshimabukuro15 <tshimabukuro15@punahou.edu>
2014-11-17 15:10:37 -1000

    A Maze Game

---
+++ Moving_Dot/Moving_Dot.ino
@@ -0,0 +1,76 @@
+/*
+ Maze Game
+ 
+ Example file using the The Meggy Jr Simplified Library (MJSL)
+  from the Meggy Jr RGB library for Arduino
+   
+Moving_Dot
+   
+   
+ 
+ Version 1.25 - 12/2/2008
+ Copyright (c) 2008 Windell H. Oskay.  All right reserved.
+ http://www.evilmadscientist.com/
+ 
+ This library is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ 	  
+ */
+
+ 
+ 
+ 
+ 
+
+/*
+ * Adapted from "Blink,"  The basic Arduino example.  
+ * http://www.arduino.cc/en/Tutorial/Blink
+ */
+
+#include <MeggyJrSimple.h>    // Required code, line 1 of 2.
+//Global variables go here
+int xcoord = 3;
+int ycoord = 4;
+
+void setup()                    // run once, when the sketch starts
+{
+  MeggyJrSimpleSetup();      // Required code, line 2 of 2.
+}
+
+void loop()                   // run over and over again
+{
+  CheckButtonsPress();
+  if (Button_Right)
+  {
+    if(xcoord < 7)
+     xcoord = xcoord + 1; // Also xcoord++;
+  }
+
+  if (Button_Left)
+  {
+    if(xcoord > 0)
+    xcoord = xcoord -1;
+  }
+  
+  if (Button_Up)
+  {
+    if (ycoord < 8)
+    ycoord = ycoord + 1;
+  }
+  if (Button_Down))
+  DrawPx(xcoord,ycoord,Red); //Draws Dot
+  DisplaySlate();
+  delay(150);
+ ClearSlate();
+}
+
